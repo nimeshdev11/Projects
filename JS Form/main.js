0f580b3340082b05b3b091ec5a2aa2df77d91170
nimeshdev11 <136144123+nimeshdev11@users.noreply.github.com>
2023-06-11 19:31:48 +0530

    Add files via upload

---
+++ JS Form/main.js
@@ -0,0 +1,54 @@
+// let submit  = document.getElementById('submit');
+// submit.addEventListener("click", function(event){
+// event.preventDefault();
+// checkData();
+
+// });
+
+// let username = document.getElementById("username");
+// function checkData(){
+// let userNameValue = username.value.trim( ); 
+ 
+// if(userNameValue == ""){
+//     setError(username, "UserName Can't  be blank ");
+
+// }
+// else{
+// setSuccess(username); 
+// }
+//  }
+
+//   function setError(U, msg){
+// let parentBox =  U.parentElement;
+// parentBox.className = "data-field error" 
+// let sapn = parentBox.querySelector('span');
+// let fa = parentBox.querySelector('.fa')
+// fa.className = ""
+
+// sapn.innerText = msg; 
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+setTimeout(show => console.log("HI i am settime out"),5000 );
+console.log("end");
+
+
+
+
+
+
+
+
